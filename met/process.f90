a5cf6447ecbdd63bbfa9861d200a2c6ab9a22b6e
Susan Allen <sallen@eoas.ubc.ca>
2015-04-25 12:04:53 -0700

    Updated Clouds to April 2015 using "bought" EC winds
    gaps patched with
    1) single point gaps, average of single value on each side
    2) multi point gaps, average of 4 values each side
    
    then linearly interpolated from 3 hour output to single hours

--- met/process.f90
+++ met/process.f90
@@ -2,20 +2,21 @@ program process
 
 ! read in the files that EC sent me in Aug 2010 and proces them to be like the previous files.  Basically reading in integers and putting out f5.1
 ! for Nov 2011 data, inserted spaces (insert rectangle) as required before running this.
+! for April 2015 data, make it read without inserting the rectangle
 
 
   integer :: id, year,month,day,code
   real*8:: data(24)
   integer :: datai(24)
 
-  npts = 6570
-
+  npts = 874
 
   do i=1,npts
-     read (*,*) id, year, month, day, code, datai
+     read (*,101) id, year, month, day, code, datai
+101  format (i7,i4,i2,i2,i3,i6,23(1x,i6))
      data = datai
-     write (*,101) id, year, month, day, code, data
-101  format (i7,1x,i4,1x,i2,1x,i2,1x,i2,1x,24(F6.1,1x))
+     write (*,102) id, year, month, day, code, data
+102 format (i7,1x,i4,1x,i2,1x,i2,1x,i2,1x,24(F8.1,1x))
   enddo
 
 end program process
